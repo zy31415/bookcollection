c115a3df573aaeb771f05c85f778f0be1d3d8c6c
Yang Zhang <zy31415@gmail.com>
2015-02-25 15:44:26 -0800

    .

--- src/bookcollection/gui/MainWindows.java
+++ src/bookcollection/gui/MainWindows.java
@@ -1,7 +1,11 @@
 package bookcollection.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+
 import bookcollection.config.DirectoryInitializer;
+
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,6 +16,11 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JFileChooser;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.ListSelectionModel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -42,13 +51,12 @@ public class MainWindows {
 		initialize();
 	}
 	
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -57,40 +65,45 @@ public class MainWindows {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmOpenDirectory = new JMenuItem("Open Directory");
-		mntmOpenDirectory.addActionListener(new OpenDirectoryActionListener());
-		mnFile.add(mntmOpenDirectory);
+		ChooseDirectoryMenuItem mntmChooseDirectory = new ChooseDirectoryMenuItem();
+		mnFile.add(mntmChooseDirectory);
 		
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mntmExit.addActionListener(new ExitActionListener());
 		mnFile.add(mntmExit);
 		
-	}
-	
-	private class OpenDirectoryActionListener implements ActionListener {
-		private JFileChooser chooser;
-		private File directoryChoosen;
+		// Create a panel to hold all other components
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout( new BorderLayout() );
 		
-		public OpenDirectoryActionListener(){
-			initChooser();
-		}
 		
-		public void actionPerformed(ActionEvent arg0) {
-			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-				directoryChoosen = chooser.getSelectedFile();
-				System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
-				} else {
-			      System.out.println("No Selection ");
-			    }
-		}
+		String	listData[] =
+			{
+				"Item 1",
+				"Item 2",
+				"Item 3",
+				"Item 4"
+			};
 		
-		private void initChooser(){
-			chooser = new JFileChooser();
-			chooser.setCurrentDirectory(new java.io.File("."));
-			chooser.setDialogTitle("Choose a directory");
-			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			chooser.setAcceptAllFileFilterUsed(false);	
-		}
+		JList list = new JList(listData);
+		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setVisibleRowCount(-1);
+		topPanel.add(list);
+		
+		//Create a split pane with the two scroll panes in it.
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+		                           topPanel, new JPanel());
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation(150);
+
+		//Provide minimum sizes for the two components in the split pane
+		Dimension minimumSize = new Dimension(100, 50);
+		topPanel.setMinimumSize(minimumSize);
+		
+		frame.getContentPane().add(splitPane);
+		
+		
 	}
 	
 	private class ExitActionListener implements ActionListener{
@@ -99,20 +112,32 @@ public class MainWindows {
 		}
 	}
 	
-	private class BookCollectionDirectoryChooser extends JFileChooser implements ActionListener{
+	private class ChooseDirectoryMenuItem extends JMenuItem implements ActionListener{
 		private File directory;
+		private JFileChooser chooser;
+		
+		public ChooseDirectoryMenuItem(){
+			super("Choose directory");
+			initChooser();
+			this.addActionListener(this);
+		}
+		
+		private void initChooser(){
+			chooser = new JFileChooser();
+			chooser.setCurrentDirectory(new java.io.File("."));
+			chooser.setDialogTitle("Choose a directory");
+			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			chooser.setAcceptAllFileFilterUsed(false);
+			chooser.addActionListener(this);
+		}
 		
 		public void actionPerformed(ActionEvent arg0) {
-			if (this.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-				directory = this.getSelectedFile();
-				System.out.println("getSelectedFile() : " + this.getSelectedFile());
+			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				directory = chooser.getSelectedFile();
+				System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
 				} else {
 			      System.out.println("No Selection ");
 			    }
-		}
-		
-		public getDirectory(){
-			return directory;
 		}
 	}
 
