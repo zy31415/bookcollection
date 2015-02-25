a1bd672c9b74e96bb372dc09608d2e8afa06d570
Yang Zhang <zy31415@gmail.com>
2015-02-25 15:02:26 -0800

    .

--- src/bookcollection/gui/MainWindows.java
+++ src/bookcollection/gui/MainWindows.java
@@ -8,6 +8,12 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JFileChooser;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainWindows {
 
@@ -27,8 +33,6 @@ public class MainWindows {
 				}
 			}
 		});
-		
-	test_sqlite3();
 	}
 
 	/**
@@ -38,39 +42,6 @@ public class MainWindows {
 		initialize();
 	}
 	
-	public static void test_sqlite3(){
-		Connection c= null;
-        Statement stmt = null;
-        
-        DirectoryInitializer dirInit;
-        
-        dirInit = new DirectoryInitializer(new File("directory"));
-        
-        dirInit.initDir();
-       
-        
-        
-        String db_file = "info.db";
-        String url = "jdbc:sqlite:"+db_file;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection(url);
-            
-            stmt = c.createStatement();
-            
-            String sql = "CREATE TABLE IF NOT EXISTS tb_tags" +
-                    "(tag TEXT," +
-                    "description TEXT)";
-            
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();            
-        } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
-    System.out.println("Opened database successfully");
-	}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -79,6 +50,73 @@ public class MainWindows {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmOpenDirectory = new JMenuItem("Open Directory");
+		mntmOpenDirectory.addActionListener(new OpenDirectoryActionListener());
+		mnFile.add(mntmOpenDirectory);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ExitActionListener());
+		mnFile.add(mntmExit);
+		
+	}
+	
+	private class OpenDirectoryActionListener implements ActionListener {
+		private JFileChooser chooser;
+		private File directoryChoosen;
+		
+		public OpenDirectoryActionListener(){
+			initChooser();
+		}
+		
+		public void actionPerformed(ActionEvent arg0) {
+			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				directoryChoosen = chooser.getSelectedFile();
+				System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
+				} else {
+			      System.out.println("No Selection ");
+			    }
+		}
+		
+		private void initChooser(){
+			chooser = new JFileChooser();
+			chooser.setCurrentDirectory(new java.io.File("."));
+			chooser.setDialogTitle("Choose a directory");
+			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			chooser.setAcceptAllFileFilterUsed(false);	
+		}
+	}
+	
+	private class ExitActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			System.exit(0);			
+		}
+	}
+	
+	private class BookCollectionDirectoryChooser extends JFileChooser implements ActionListener{
+		private File directory;
+		
+		public void actionPerformed(ActionEvent arg0) {
+			if (this.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				directory = this.getSelectedFile();
+				System.out.println("getSelectedFile() : " + this.getSelectedFile());
+				} else {
+			      System.out.println("No Selection ");
+			    }
+		}
+		
+		public getDirectory(){
+			return directory;
+		}
 	}
 
 }
+
+
+
--- src/bookcollection/sqlite/SQLite.java
+++ src/bookcollection/sqlite/SQLite.java
@@ -1,5 +1,45 @@
 package bookcollection.sqlite;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import bookcollection.config.DirectoryInitializer;
+
 public class SQLite {
+	public static void test_sqlite3(){
+		Connection c= null;
+        Statement stmt = null;
+        
+        DirectoryInitializer dirInit;
+        
+        dirInit = new DirectoryInitializer(new File("directory"));
+        
+        dirInit.initDir();
+       
+        
+        
+        String db_file = "info.db";
+        String url = "jdbc:sqlite:"+db_file;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(url);
+            
+            stmt = c.createStatement();
+            
+            String sql = "CREATE TABLE IF NOT EXISTS tb_tags" +
+                    "(tag TEXT," +
+                    "description TEXT)";
+            
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();            
+        } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Opened database successfully");
+	}
 
 }
